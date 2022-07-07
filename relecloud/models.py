a6611a00bcd574d9c2b603831d2fbb689764b174
eungyukm <eungyukm@gmail.com>
2022-07-07 14:43:52 +0900

    upload data base

--- relecloud/models.py
+++ relecloud/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Destination(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cruise(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    destinations = models.ManyToManyField(
+        Destination,
+        related_name='destinations'
+    )
+
+    def __str__(self) -> str:
+        return self.name
--- relecloud/views.py
+++ relecloud/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    return HttpResponse('욕쟁이 유진 & 지혜')
