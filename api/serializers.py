ad927733df743400c46b11c2ccac73894d5e886b
eungyukm <eungyukm@gmail.com>
2022-07-13 09:10:21 +0900

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from user_app.models import UserTable
 
 
 class UserSerializer(serializers.ModelSerializer):
-    user_idx = serializers.IntegerField(read_only=True)
+    # user_idx = serializers.IntegerField(read_only=True)
     user_name = serializers.CharField(
         required=False, allow_blank=True, max_length=100)
     user_id = serializers.CharField(
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
         return UserTable.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.user_idx = validated_data.get('user_idx', instance.user_idx)
+        # instance.user_idx = validated_data.get('user_idx', instance.user_idx)
         instance.user_name = validated_data.get(
             'user_name', instance.user_name)
         instance.user_id = validated_data.get('user_id', instance.user_id)
