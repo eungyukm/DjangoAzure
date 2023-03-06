e8fa21f42cb9f78b82adaac4aae788b0ac22746c
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2023-03-06 11:12:26 +0900

    upload

--- api/views.py
+++ api/views.py
@@ -162,26 +162,24 @@ def api_profile_result(request):
     body = json.loads(request.body)
     print(body)
     print(request.data)
-    user_date = body['date']
-    user_fps = body['fps']
-    print(user_date)
-    print(user_fps)
+    user_id = body['user_id']
+    user_pw = body['user_pw']
+    print(user_id)
+    print(user_pw)
+    try:
+        user_model = user_app.models.UserTable.objects.get(user_id=user_id)
+        # print(user_model)
 
-    
-    # try:
-    #     user_model = user_app.models.UserTable.objects.get(user_id=user_id)
-    #     # print(user_model)
-
-    #     # 로그인한 사용자와 데이터베이스에서 가져온 데이터의 비밀번호가 같을 경우
-    #     if user_pw == user_model.user_pw:
-    #         # 로그인에 성공할 경우 세셔에 로그인 여부값을 저장합니다.
-    #         res_message = '200'
-    #         return Response(res_message, status=status.HTTP_200_OK)
-    #     # 비밀번호가 다를 경우
-    #     else:
-    #         res_message = '210'
-    #         return Response(res_message, status=status.HTTP_200_OK)
-    # except:
-    #     # 아이디가 없는 경우
-    #     res_message = '220'
-    #     return Response(res_message, status=status.HTTP_200_OK)
+        # 로그인한 사용자와 데이터베이스에서 가져온 데이터의 비밀번호가 같을 경우
+        if user_pw == user_model.user_pw:
+            # 로그인에 성공할 경우 세셔에 로그인 여부값을 저장합니다.
+            res_message = '200'
+            return Response(res_message, status=status.HTTP_200_OK)
+        # 비밀번호가 다를 경우
+        else:
+            res_message = '210'
+            return Response(res_message, status=status.HTTP_200_OK)
+    except:
+        # 아이디가 없는 경우
+        res_message = '220'
+        return Response(res_message, status=status.HTTP_200_OK)
